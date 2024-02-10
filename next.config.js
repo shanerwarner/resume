0618766248cd6a732577c03277e990f4ac5d6a11
foxtrotoscarvictor <foxtrotoscarvictor@gmail.com>
2024-02-10 17:19:48 +0530

    Site Test3

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
+
 const nextConfig = {}
 
 module.exports = nextConfig;
+
+const config = {
+  experimental: {
+    serverActions: true,
+  },
+};
+
+module.exports = config;
